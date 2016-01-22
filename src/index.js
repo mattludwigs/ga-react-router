a7dd721865058de52865a07101b9e58add9dbdfc
Matthew Ludwigs <matthew.ludwigs@colonyamerican.com>
2016-01-22 14:03:15 -0700

    add options back in

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 'use strict';
 var ga = require('./ga');
 
-
 function analytics(state, options) {
   if (!options) {
     options = {};
   }
   options.page = state.pathname;
+
   ga('send', {
     hitType: 'pageview',
     page: options.pathname,
     location: window.location.href
-  }, options);
+  });
 }
 
 
